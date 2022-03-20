7c4a298b17ff0bf81c914788173eb6e55c1b10a9
Abourass <Abourassa@AssetVal.com>
2022-03-20 14:11:32 -0600

    :art: Start adding config for item sheets

--- modules/sheets/itemSheet.js
+++ modules/sheets/itemSheet.js
@@ -1,4 +1,21 @@
 export default class WonderItemSheet extends ItemSheet {
+  /**
+   * Options for the UI composition
+   * This will make Item Sheets open larger
+   * and with the classes we want
+   */
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      width: 600,
+      height: 600,
+      // resizable: false,
+      // draggable: false,
+      classes: ['wondershade', 'sheet', 'item'],
+      // scrollY: ['.sheet-body'],
+    });
+  }
+
+  // Point to the correct HTML depending on the item type
   get template(){
     return `systems/wondershade/templates/sheets/items/${this.item.data.type}-sheet.html`;
   }
