40a5baeb80df94f0edfba488b2d902f133287f27
Abourass <Abourassa@AssetVal.com>
2022-03-20 22:16:53 -0600

    :art: Start working on handlebars

--- wonderSystem.mjs
+++ wonderSystem.mjs
@@ -2,6 +2,14 @@ import { wondershadeConf } from './modules/config.js';
 import WonderItemSheet from './modules/sheets/itemSheet.js';
 import WonderActorSheet from './modules/sheets/actorSheet.js';
 
+async function preloadHBSTemplates(){
+  const templatePaths = [
+    'systems/wondershade/templates/partials/layout/backgroundInfo.hbs',
+  ];
+
+  return loadTemplates(templatePaths);
+}
+
 Hooks.once('init', () => {
   console.log('Wonder System loading...');
 
@@ -14,4 +22,6 @@ Hooks.once('init', () => {
   // Unregister the default item sheet
   Actors.unregisterSheet('core', ActorSheet);
   Actors.registerSheet('wondershade', WonderActorSheet, { makeDefault: true });
+
+  preloadHBSTemplates();
 });
