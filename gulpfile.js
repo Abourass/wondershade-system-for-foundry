6cd1b21d3afd4f6647a1753395f7c05a11ded125
Abourass <Abourassa@AssetVal.com>
2022-03-25 15:04:55 -0600

    :art: Trying to add some vendor prefixing

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 const gulp = require('gulp');
 const less = require('gulp-less');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('less', (cb) => {
   gulp
-    .src('/styles/src/*.less')
+    .src('./styles/src/index.less')
     .pipe(less())
+    .pipe(
+      autoprefixer({ cascade: false }),
+    )
     .pipe(
       gulp.dest(f => `/styles/${f.base}`),
     );
