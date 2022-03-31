5ecdda0a1bb5706d0dd910789251792eb9d2c5de
Abourass <Abourassa@AssetVal.com>
2022-03-30 22:26:49 -0600

    :sparkles: Thank's to the help of @KyleHagad drag and drop is working with items

--- modules/sheets/actorSheet.js
+++ modules/sheets/actorSheet.js
@@ -21,24 +21,93 @@ export default class WonderActorSheet extends ActorSheet {
 
   getData() {
     // Execute the default getData() method
-    const data = super.getData();
-    console.log(data);
-    // Attach the localization to the data
-    data.config = CONFIG.wondershade;
+    const ctx = super.getData();
+    console.log('[WonderSystem:CTX]', ctx);
+
+    // Create a safe clone of the actor data
+    const actorData = this.actor.data.toObject(false);
+
+    // Attach the localization to the ctx
+    ctx.config = CONFIG.wondershade;
     // Let's alias actor.data.data since it's tedious to access
-    data.actorData = data.actor.data.data;
+    ctx.actorData = actorData.data;
 
-    if (data.actorData.firstOpen){
-      const maxHealth = Math.round(((data.actorData.abilities.str.value / 5) + (data.actorData.abilities.con.value / 5)) * 1.5);
-      // lets set their current health
-      data.actorData.attributes.hp.value = maxHealth;
-      // Lets set their max health
-      data.actorData.attributes.hp.max = maxHealth;
+    this._prepareItems(ctx);
 
-      data.actorData.firstOpen = false;
+    // if (data.actorData.firstOpen){
+    //   const maxHealth = Math.round(((data.actorData.abilities.str.value / 5) + (data.actorData.abilities.con.value / 5)) * 1.5);
+    //   // lets set their current health
+    //   data.actorData.attributes.hp.value = maxHealth;
+    //   // Lets set their max health
+    //   data.actorData.attributes.hp.max = maxHealth;
+
+    //   data.actorData.firstOpen = false;
+    // }
+
+    return ctx;
+  }
+
+  _prepareItems(ctx) {
+    // Create Arrays to store the User-Specific Items In
+    const weapons = [];
+    const equipment = [];
+    const consumables = [];
+    const tools = [];
+    const loot = [];
+    const classFeatures = [];
+    const feats = [];
+    const backpack = [];
+    const spells = {
+      cantrips: [],
+      wonderSpells: [],
+      firstLevel: [],
+      secondLevel: [],
+      thirdLevel: [],
+      fourthLevel: [],
+      fifthLevel: [],
+      sixthLevel: [],
+      seventhLevel: [],
+      eighthLevel: [],
+      ninthLevel: [],
+    };
+
+    // Iterate through the items, assigning them to their appropriate array
+    for (const item of ctx.items) {
+      if (item.type === 'weapon') weapons.push(item);
+      if (item.type === 'equipment') equipment.push(item);
+      if (item.type === 'consumable') consumables.push(item);
+      if (item.type === 'tool') tools.push(item);
+      if (item.type === 'loot') loot.push(item);
+      if (item.type === 'classFeature') classFeatures.push(item);
+      if (item.type === 'feat') feats.push(item);
+      if (item.type === 'spell'){
+        switch (item.data.level) {
+          case 'cantrip': spells.cantrips.push(item); break;
+          case 'wonder': spells.wonderSpells.push(item); break;
+          case 1: spells.firstLevel.push(item); break;
+          case 2: spells.secondLevel.push(item); break;
+          case 3: spells.thirdLevel.push(item); break;
+          case 4: spells.fourthLevel.push(item); break;
+          case 5: spells.fifthLevel.push(item); break;
+          case 6: spells.sixthLevel.push(item); break;
+          case 7: spells.seventhLevel.push(item); break;
+          case 8: spells.eighthLevel.push(item); break;
+          case 9: spells.ninthLevel.push(item); break;
+        }
+      }
+      if (item.type === 'backpack') backpack.push(item);
     }
 
-    return data;
+    // Assign the user specific data to the context
+    ctx.weapons = weapons;
+    ctx.equipment = equipment;
+    ctx.consumables = consumables;
+    ctx.tools = tools;
+    ctx.loot = loot;
+    ctx.classFeatures = classFeatures;
+    ctx.feats = feats;
+    ctx.spells = spells;
+    ctx.backpack = backpack;
   }
 
   activateListeners(html) {
