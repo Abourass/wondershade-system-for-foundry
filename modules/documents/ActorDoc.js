cc18c60c61adb1d60243bea27fad0fd2b8ec7542
Abourass <Abourassa@AssetVal.com>
2022-04-20 21:57:52 -0600

    :art: More fixes

--- modules/documents/ActorDoc.js
+++ modules/documents/ActorDoc.js
@@ -1,3 +1,10 @@
+/**
+ *
+ * @param {number} value
+ * @param {number} min The minimum the value can be returned as
+ * @param {number} max The maximum the value can be returned as
+ * @returns {number}
+ */
 Math.keepInsideRange = function(value, min, max) {
   if (typeof value !== 'number' || typeof min !== 'number' || typeof max !== 'number') {
     throw new Error('Math.keepInsideRange: value, min and max must be numbers');
@@ -20,32 +27,48 @@ export default class WonderActor extends Actor {
     super.prepareData();
   }
 
-  _getSuccessAndFailure(value, difficulty){
+  /**
+   *
+   * @param {number} value
+   * @param {number} difficulty
+   * @param {number} innateLuck
+   * @returns {{success: number, hardSuccess: number, criticalSuccess: number, hardFailure: number, criticalFailure: number}}
+   */
+  _getSuccessAndFailure(value, difficulty, innateLuck){
+    // Typecast the attributes if they are not already numbers
     if (typeof value !== 'number') Number(value);
-    // Grab the actors roll difficulty
     if (typeof difficulty !== 'number') difficulty = Number(difficulty);
+    if (typeof innateLuck !== 'number') innateLuck = Number(innateLuck);
     // Success is equal to the skill's value minus the difficulty
-    const success = Math.keepInsideRange(value + difficulty, 1, 100);
-    // Hard Success is 33% of success
+    const success = Math.keepInsideRange((value + difficulty + innateLuck), 1, 100);
+    // Hard Success is 25% of success
     const hardSuccess = (success !== 1)
-      ? Math.keepInsideRange(Math.floor(success / 3), 1, 100)
+      ? Math.keepInsideRange(Math.floor(success / 4), 1, 100)
       : 0;
-    // Crit success is 14.28% of success
+    // Crit success is 12.5% of success
     let criticalSuccess = (success !== 1)
-      ? Math.keepInsideRange(Math.floor(success / 7), 1, 100)
+      ? Math.keepInsideRange(Math.floor(success / 8), 1, 100)
       : 0;
     // if hard success and crit success are the same subtract one from crit success
     if (hardSuccess === criticalSuccess) --criticalSuccess;
-    // Hard Failure is 100 - 33% lack of skill
-    const hardFailure = Math.keepInsideRange(100 - Math.floor((100 - success) / 3), 1, 100);
-    // Critical Failure is 100 - 14.28% lack of skill
-    const criticalFailure = Math.keepInsideRange(100 - Math.floor((100 - success) / 7), 1, 100);
+    // Hard Failure is 100 - 28% lack of skill (easier to fuck up in life)
+    const hardFailure = Math.keepInsideRange(100 - Math.floor((100 - success) / 3.5), 1, 100);
+    // Critical Failure is 100 - 12.5% lack of skill
+    const criticalFailure = Math.keepInsideRange(100 - Math.floor((100 - success) / 8), 1, 100);
 
     return { success, hardSuccess, criticalSuccess, hardFailure, criticalFailure };
   }
 
-  _getOutcome(computedRoll, value, difficulty){
-    const { success, hardSuccess, criticalSuccess, hardFailure, criticalFailure } = this._getSuccessAndFailure(value, difficulty);
+  /**
+   *
+   * @param {number} computedRoll
+   * @param {number} value
+   * @param {number} difficulty
+   * @param {number} innateLuck
+   * @returns {'Success'|'Hard Success'|'Critical Success'|'Failure'|'Hard Failure'|'Critical Failure'}
+   */
+  _getOutcome(computedRoll, value, difficulty, innateLuck){
+    const { success, hardSuccess, criticalSuccess, hardFailure, criticalFailure } = this._getSuccessAndFailure(value, difficulty, innateLuck);
 
     if (computedRoll <= success) {
       if (computedRoll <= hardSuccess) {
@@ -63,7 +86,7 @@ export default class WonderActor extends Actor {
 
   /**
    * Handle clickable rolls.
-   * @param {Event} event   The originating click event
+   * @param {Event} ctx
    * @private
    */
   async roll(ctx) {
@@ -73,13 +96,15 @@ export default class WonderActor extends Actor {
     const willpower = Number(actor.data.attributes.willpower);
     // Grab the characters difficulty
     const difficulty = Number(actor.data.attributes.difficulty);
+    // Grab I. Luck
+    const innateLuck = Number(actor.data.abilities.iLuck.value);
     // Get the success and failure values for this skill
-    const { success, hardSuccess, criticalSuccess, hardFailure, criticalFailure } = this._getSuccessAndFailure(ctx.value, difficulty);
+    const { success, hardSuccess, criticalSuccess, hardFailure, criticalFailure } = this._getSuccessAndFailure(ctx.value, difficulty, innateLuck);
 
     await ctx.roll.evaluate({ async: true });
 
     const computedRoll = Math.keepInsideRange(Number(ctx.roll.result) + willpower, 1, 100);
-    const rollOutcome = this._getOutcome(computedRoll, ctx.value, difficulty);
+    const rollOutcome = this._getOutcome(computedRoll, ctx.value, difficulty, innateLuck);
 
     if (rollOutcome === 'Critical Failure'){
       ctx.roll.dice[0].options.sfx = { specialEffect: 'PlayAnimationImpact' };
@@ -119,6 +144,7 @@ export default class WonderActor extends Actor {
       },
       difficulty: Number(actor.data.attributes.difficulty),
       willpower,
+      innateLuck,
     };
 
     chatData.content = await renderTemplate(this.chatTemplates.skill, cardData);
