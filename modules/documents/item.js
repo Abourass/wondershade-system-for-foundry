7aadcce81b9d4f1ecab6d11dfcc0eca00b20c7df
Abourass <Abourassa@AssetVal.com>
2022-04-21 19:22:39 -0600

    :art: Quick hot fix to make spells better

--- modules/documents/item.js
+++ modules/documents/item.js
@@ -22,99 +22,99 @@ export default class WonderItem extends Item {
     super.prepareData();
   }
 
-  // /**
-  //  * Augment the basic Item data model with additional dynamic data.
-  //  */
-  // prepareDerivedData() {
-  //   super.prepareDerivedData();
+  /**
+   * Augment the basic Item data model with additional dynamic data.
+   */
+  prepareDerivedData() {
+    super.prepareDerivedData();
 
-  //   // Get the Item's data
-  //   const itemData = this.data;
-  //   console.debug('[WonderItem:prepareDerivedData()] -> itemData', itemData);
-  //   const data = itemData.data;
-  //   console.debug('[WonderItem:prepareDerivedData()] -> GLOBAL CONFIG', CONFIG);
-  //   const config = CONFIG.wondershade;
-  //   // console.debug('[WonderItem] prepareDerivedData', itemData);
-  //   console.debug('[WonderItem:prepareDerivedData()] this', this);
-  //   this.labels = {};
-  //   const labels = this.labels;
+    // Get the Item's data
+    const itemData = this.data;
+    console.debug('[WonderItem:prepareDerivedData()] -> itemData', itemData);
+    const data = itemData.data;
+    console.debug('[WonderItem:prepareDerivedData()] -> GLOBAL CONFIG', CONFIG);
+    const config = CONFIG.wondershade;
+    // console.debug('[WonderItem] prepareDerivedData', itemData);
+    console.debug('[WonderItem:prepareDerivedData()] this', this);
+    this.labels = {};
+    const labels = this.labels;
 
-  //   // // Classes
-  //   // if (itemData.type === 'class') {
-  //   //   data.levels = Math.clamped(data.levels, 1, 20);
-  //   // }
+    // // Classes
+    // if (itemData.type === 'class') {
+    //   data.levels = Math.clamped(data.levels, 1, 20);
+    // }
 
-  //   // // Spell Level,  School, and Components
-  //   // if (itemData.type === 'spell') {
-  //   //   data.preparation.mode = data.preparation.mode || 'prepared';
-  //   //   labels.level = config.spellLevels[data.level];
-  //   //   labels.school = config.spellSchools[data.school];
-  //   //   labels.components = Object.entries(data.components).reduce((arr, component) => {
-  //   //     if (component[1] !== true) return arr;
-  //   //     arr.push(component[0].titleCase().slice(0, 1));
-  //   //     return arr;
-  //   //   }, []);
-  //   //   labels.materials = data?.materials?.value ?? null;
-  //   // } else if (itemData.type === 'feat') { // Feat Items
-  //   //   const act = data.activation;
-  //   //   if (act && (act.type === config.abilityActivationTypes.legendary)){
-  //   //     labels.featType = game.i18n.localize('WonderSystem.LegendaryActionLabel');
-  //   //   } else if (act && (act.type === config.abilityActivationTypes.lair)){
-  //   //     labels.featType = game.i18n.localize('WonderSystem.LairActionLabel');
-  //   //   } else if (act && act.type){
-  //   //     labels.featType = game.i18n.localize(data.damage.length ? 'WonderSystem.Attack' : 'WonderSystem.Action');
-  //   //   } else { labels.featType = game.i18n.localize('WonderSystem.Passive'); }
-  //   // } else if (itemData.type === 'equipment') { // Equipment Items
-  //   //   labels.armor = data.armor.value ? `${data.armor.value} ${game.i18n.localize('WonderSystem.AC')}` : '';
-  //   // }
+    // Spell Level,  School, and Components
+    if (itemData.type === 'spell') {
+      data.preparation.mode = data.preparation.mode || 'prepared';
+      labels.level = config.spellLevels[data.level];
+      labels.school = config.spellSchools[data.school];
+      labels.components = Object.entries(data.components).reduce((arr, component) => {
+        if (component[1] !== true) return arr;
+        arr.push(component[0].titleCase().slice(0, 1));
+        return arr;
+      }, []);
+      labels.materials = data?.materials?.value ?? null;
+    } else if (itemData.type === 'feat') { // Feat Items
+      const act = data.activation;
+      if (act && (act.type === config.abilityActivationTypes.legendary)){
+        labels.featType = game.i18n.localize('WonderSystem.LegendaryActionLabel');
+      } else if (act && (act.type === config.abilityActivationTypes.lair)){
+        labels.featType = game.i18n.localize('WonderSystem.LairActionLabel');
+      } else if (act && act.type){
+        labels.featType = game.i18n.localize(data.damage.length ? 'WonderSystem.Attack' : 'WonderSystem.Action');
+      } else { labels.featType = game.i18n.localize('WonderSystem.Passive'); }
+    } else if (itemData.type === 'equipment') { // Equipment Items
+      labels.armor = data.armor.value ? `${data.armor.value} ${game.i18n.localize('WonderSystem.AC')}` : '';
+    }
 
-  //   // // Activated Items
-  //   // if (data.hasOwnProperty('activation')) {
-  //   //   // Ability Activation Label
-  //   //   const act = data.activation || {};
-  //   //   // console.debug('[WonderItem] config', config);
-  //   //   if (act) labels.activation = [act.cost, config.abilityActivationTypes[act.type]].filterJoin(' ');
+    // // Activated Items
+    // if (data.hasOwnProperty('activation')) {
+    //   // Ability Activation Label
+    //   const act = data.activation || {};
+    //   // console.debug('[WonderItem] config', config);
+    //   if (act) labels.activation = [act.cost, config.abilityActivationTypes[act.type]].filterJoin(' ');
 
-  //   //   // Target Label
-  //   //   const tgt = data.target || {};
-  //   //   if (['none', 'touch', 'self'].includes(tgt.units)) tgt.value = null;
-  //   //   if (['none', 'self'].includes(tgt.type)) {
-  //   //     tgt.value = null;
-  //   //     tgt.units = null;
-  //   //   }
-  //   //   labels.target = [tgt.value, config.distanceUnits[tgt.units], config.targetTypes[tgt.type]].filterJoin(' ');
+    //   // Target Label
+    //   const tgt = data.target || {};
+    //   if (['none', 'touch', 'self'].includes(tgt.units)) tgt.value = null;
+    //   if (['none', 'self'].includes(tgt.type)) {
+    //     tgt.value = null;
+    //     tgt.units = null;
+    //   }
+    //   labels.target = [tgt.value, config.distanceUnits[tgt.units], config.targetTypes[tgt.type]].filterJoin(' ');
 
-  //   //   // Range Label
-  //   //   const rng = data.range || {};
-  //   //   if (['none', 'touch', 'self'].includes(rng.units)) {
-  //   //     rng.value = null;
-  //   //     rng.long = null;
-  //   //   }
-  //   //   labels.range = [rng.value, rng.long ? `/ ${rng.long}` : null, config.distanceUnits[rng.units]].filterJoin(' ');
+    //   // Range Label
+    //   const rng = data.range || {};
+    //   if (['none', 'touch', 'self'].includes(rng.units)) {
+    //     rng.value = null;
+    //     rng.long = null;
+    //   }
+    //   labels.range = [rng.value, rng.long ? `/ ${rng.long}` : null, config.distanceUnits[rng.units]].filterJoin(' ');
 
-  //   //   // Duration Label
-  //   //   const dur = data.duration || {};
-  //   //   if (['inst', 'perm'].includes(dur.units)) dur.value = null;
-  //   //   labels.duration = [dur.value, config.timePeriods[dur.units]].filterJoin(' ');
+    //   // Duration Label
+    //   const dur = data.duration || {};
+    //   if (['inst', 'perm'].includes(dur.units)) dur.value = null;
+    //   labels.duration = [dur.value, config.timePeriods[dur.units]].filterJoin(' ');
 
-  //   //   // Recharge Label
-  //   //   const chg = data.recharge || {};
-  //   //   labels.recharge = `${game.i18n.localize('WonderSystem.Recharge')} [${chg.value}${parseInt(chg.value, 10) < 6 ? '+' : ''}]`;
-  //   // }
+    //   // Recharge Label
+    //   const chg = data.recharge || {};
+    //   labels.recharge = `${game.i18n.localize('WonderSystem.Recharge')} [${chg.value}${parseInt(chg.value, 10) < 6 ? '+' : ''}]`;
+    // }
 
-  //   // // Item Actions
-  //   // if (data.hasOwnProperty('actionType')) {
-  //   //   // Damage
-  //   //   const dam = data.damage || {};
-  //   //   if (dam.parts) {
-  //   //     labels.damage = dam.parts.map(d => d[0]).join(' + ').replace(/\+ -/g, '- ');
-  //   //     labels.damageTypes = dam.parts.map(d => config.damageTypes[d[1]]).join(', ');
-  //   //   }
-  //   // }
+    // // Item Actions
+    // if (data.hasOwnProperty('actionType')) {
+    //   // Damage
+    //   const dam = data.damage || {};
+    //   if (dam.parts) {
+    //     labels.damage = dam.parts.map(d => d[0]).join(' + ').replace(/\+ -/g, '- ');
+    //     labels.damageTypes = dam.parts.map(d => config.damageTypes[d[1]]).join(', ');
+    //   }
+    // }
 
-  //   // // If this item is owned, we prepareFinalAttributes() at the end of actor init
-  //   // if (!this.isOwned) this.prepareFinalAttributes();
-  // }
+    // // If this item is owned, we prepareFinalAttributes() at the end of actor init
+    // if (!this.isOwned) this.prepareFinalAttributes();
+  }
 
   // /**
   //  * Populate a label with the compiled and simplified damage formula based on owned item
